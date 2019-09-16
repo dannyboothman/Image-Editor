e1493dd6d5c1921c39489ddc853757e08154a90e
Daniel Boothman <dannyboothman@gmail.com>
2019-09-16 22:21:59 +0100

    Now possible to give download file a name and a file type

--- js/save.js
+++ js/save.js
@@ -8,7 +8,6 @@ function openSaveModal(){
     //document.querySelector("html, body").scrollTop = 0;
     setTimeout(function(){
         html2canvas(document.querySelector("#favicon_creator_visual")).then(canvas => {
-            console.log("here")
             document.querySelector("#main_save_modal_example").appendChild(canvas)
             document.querySelector("#main_save_modal_loaded").style.display = "flex";
             document.querySelector("#main_save_modal_loading").style.display = "none";
@@ -32,8 +31,18 @@ function selectDownloadFileType(el){
 
 function download(){
 
+    var fileName = document.querySelector("#download_name").value.trim();
+    if (fileName === null || fileName.length === 0){
+        fileName = "ClubSketch-" + Date.now();
+    }
+
+    var fileType = document.querySelector(".download_file_type_item_selected").getAttribute("data-type");
+    if (fileType == null || fileType.length === 0){
+        fileType = "png";
+    }
+
     var canvas = document.querySelector("#main_save_modal_example canvas");
     canvas.toBlob(function(blob) {
-        saveAs(blob, "pretty image.png");
+        saveAs(blob, fileName + "." + fileType);
     });
 }
